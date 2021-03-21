5f4f49c36443d9430750a5996ea6637991c824bb
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-21 16:26:40 -0700

    Chapter 2 Understanding Core JUnit 5 -Add private constructor -Change test display name

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -1,11 +1,17 @@
 package bookstoread;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.DisplayName;
 import java.util.List;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("<= BookShelf Specification =>")
 public class BookShelfSpec {
+
+    private BookShelfSpec(TestInfo testInfo) {
+        System.out.println("Working on test " + testInfo.getDisplayName());
+    }
 
     @Test
     public void shelfEmptyWhenNoBookAdded() throws Exception {
