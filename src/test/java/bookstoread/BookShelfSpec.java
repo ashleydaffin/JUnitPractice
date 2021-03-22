83c25d0337ad3af6b60b85721d0d933b6a05cf84
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-21 18:55:27 -0700

    Chapter 3 - Bookstoread Application. Extract duplicated code with @BeforeEach

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -1,4 +1,5 @@
 package bookstoread;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import java.util.List;
@@ -9,6 +10,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("A bookshelf")
 public class BookShelfSpec {
 
+    private BookShelf shelf;
+
+    @BeforeEach
+    void init() throws Exception{
+        shelf = new BookShelf();
+    }
+
     private BookShelfSpec(TestInfo testInfo) {
         System.out.println("From constructor...Working on test " + testInfo.getDisplayName());
     }
@@ -17,14 +25,12 @@ public class BookShelfSpec {
     @DisplayName("is empty when no book is added to it")
     public void shelfEmptyWhenNoBookAdded(TestInfo testInfo) throws Exception {
         System.out.println("From test method...Working on test " + testInfo.getDisplayName());
-        BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should be empty.");
     }
 
     @Test
     void bookshelfContainsTwoBooksWhenTwoBooksAdded() {
-        BookShelf shelf = new BookShelf();
         shelf.add("Effective Java", "Code Complete");
         List<String> books = shelf.books();
         assertEquals(2, books.size(), () -> "BookShelf should have two books.");
@@ -32,7 +38,6 @@ public class BookShelfSpec {
 
     @Test
     public void emptyBookShelfWhenAddIsCalledWithoutBooks() {
-        BookShelf shelf = new BookShelf();
         shelf.add();
         List<String> books = shelf.books();
         assertTrue(books.isEmpty(), () -> "BookShelf should empty");
@@ -41,7 +46,6 @@ public class BookShelfSpec {
 
     @Test
     void booksReturnedFromBookShelfIsImmutableForClient() {
-        BookShelf shelf = new BookShelf();
         shelf.add("Effective Java", "Code Complete");
         List<String> books = shelf.books();
         try {
