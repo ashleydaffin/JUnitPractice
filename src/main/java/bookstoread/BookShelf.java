0f1205f857a2cfffc38283d452575d36f5b4d143
Ashley Daffin <ashleyedaffin@gmail.com>
2021-03-21 20:47:31 -0700

    Chapter 3 - Add arrange() method that supports criteria. Code compiles but fails.

--- src/main/java/bookstoread/BookShelf.java
+++ src/main/java/bookstoread/BookShelf.java
@@ -5,16 +5,19 @@ import java.util.stream.Collectors;
 
 public class BookShelf {
 
-    private final List<String> books = new ArrayList<>();
+    private final List<Book> books = new ArrayList<>();
 
-    public List<String> books() {
+    public List<Book> books() {
         return Collections.unmodifiableList(books);
     }
-    public void add(String... booksToAdd) {
+    public void add(Book... booksToAdd) {
         Arrays.stream(booksToAdd).forEach(books::add);
     }
 
-    public List<String> arrange() {
+    public List<Book> arrange() {
         return books.stream().sorted().collect(Collectors.toList());
+    }
+    public List<Book> arrange(Comparator<Book> criteria){
+        return null;
     }
 }
